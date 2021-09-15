721cfbfce072dada49bbc1b3d2c63c746c92dd20
Kirill Sokirka <sokirka2003@gmail.com>
2021-09-15 15:40:41 +0300

    Create a project, desigh for layout page

---
+++ NoteWebApp/NoteWebApp/App_Start/BundleConfig.cs
@@ -0,0 +1,30 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace NoteWebApp
+{
+    public class BundleConfig
+    {        
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/Scripts").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/Scripts").Include(
+                        "~/Scripts/jquery.validate*"));
+          
+            bundles.Add(new ScriptBundle("~/Scripts").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/Scripts").Include(
+                      "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/App_LocalResourses/css")
+                .Include("~/Content/App_LocalResourses/localStyleSheet.css"));
+        }
+    }
+}
